c71ec1b69608f5bcc7c998f8cafc2f930f04d518
Nayraiyt <naomimorganc@gmail.com>
2026-04-30 11:18:52 -0400

    Added clouds

--- app/routes/hero/hero.tsx
+++ app/routes/hero/hero.tsx
@@ -9,9 +9,21 @@ import m6 from "../../assets/mini-mush-6.png";
 import sideDark from "../../assets/hero-sidebar.png";
 import titleLogo from "../../assets/title-logo.png";
 
+import cloud1 from "../../assets/cloud-1.png";
+import cloud2 from "../../assets/cloud-2.png";
+import cloud3 from "../../assets/cloud-3.png";
+import cloud4 from "../../assets/cloud-4.png";
+
 function Hero() {
     return(
         <div className = "hero">
+            <div className = "clouds">
+                <img className = "cloud cloud-1" src={cloud1} />
+                <img className = "cloud cloud-2" src={cloud2}/>
+                <img className = "cloud cloud-3" src={cloud3}/>
+                <img className = "cloud cloud-4" src={cloud4}/>
+            </div>
+
             <div className = "hills">
                 <div className = "back-hill"></div>
                 <div className = "mid-hill"></div>
