96251920bbbaa5ef903435cc616457768995c0a3
Zoey Chen <zixiu.zoey.chen@gmail.com>
2026-05-01 09:11:20 -0400

    added springboot backend

---
+++ mushroot-backend/src/main/java/mushroot/mushrootbackend/MushrootBackendApplication.java
@@ -0,0 +1,13 @@
+package mushroot.mushrootbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MushrootBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MushrootBackendApplication.class, args);
+	}
+
+}
