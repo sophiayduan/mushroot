e50caba40a00699eae36ca2920b4b95a56281d20
Nayraiyt <naomimorganc@gmail.com>
2026-05-02 12:36:17 -0400

    added @media

--- app/routes.ts
+++ app/routes.ts
@@ -1,6 +1,5 @@
 import { type RouteConfig, route, index } from "@react-router/dev/routes";
 
 export default [
-    index("routes/merge/merge.tsx"),
-
+    index("routes/hero/hero.tsx"),
 ] satisfies RouteConfig;
