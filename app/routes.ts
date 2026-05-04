eecd8b718a583dbba7ed87b3df3aa429d9b9efc2
sophiayduan <sophiayduan@gmail.com>
2026-05-04 11:31:02 -0400

    add dash and tailwind css

--- app/routes.ts
+++ app/routes.ts
@@ -2,4 +2,5 @@ import { type RouteConfig, route, index } from "@react-router/dev/routes";
 
 export default [
     index("routes/hero/hero.tsx"),
+    route("dashboard", "routes/dashboard/dashboard.tsx"),
 ] satisfies RouteConfig;
