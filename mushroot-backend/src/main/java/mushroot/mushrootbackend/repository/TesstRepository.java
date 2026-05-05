0e32d307be3c3dece529f59053c6acbc65579993
AnnieL5 <atianyi.liang@gmail.com>
2026-05-05 11:07:17 -0400

    database

--- mushroot-backend/src/main/java/mushroot/mushrootbackend/repository/TesstRepository.java
+++ mushroot-backend/src/main/java/mushroot/mushrootbackend/repository/TesstRepository.java
@@ -5,24 +5,24 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 // JpaRepository gives you free methods: save(), findById(), findAll(), delete()...
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface TestRepository extends JpaRepository<Test, Long> {
 
     // Spring reads this method NAME and writes the SQL for you automatically!
     // It becomes: SELECT * FROM users WHERE email = ?
     // 1. Find by ID (Already exists in JpaRepository, but for clarity:)
-    Optional<User> findById(Long id);
+    Optional<Test> findById(Long id);
 
     // 2. Find by Course Code (using the Enum)
-    Optional<User> findByCourseCode(Course_Code courseCode);
+    List<Test> findByCourseCode(Course_Code courseCode);
 
     // 3. Find by Year
-    List<User> findByYear(int year);
+    List<Test> findByYear(int year);
 
     // 4. Find by Teacher Name
-    List<User> findByTeacherName(String teacherName);
+    List<Test> findByTeacherName(String teacherName);
     
     // Bonus: Find by multiple criteria (e.g., Year and Teacher)
-    List<User> findByYearAndTeacherName(int year, String teacherName);
+    List<Test> findByYearAndTeacherName(int year, String teacherName);
 
-    List<User> findByCourseCodeAndTeacherName(Course_Code courseCode, String teacherName);
+    List<Test> findByCourseCodeAndTeacherName(Course_Code courseCode, String teacherName);
 }
