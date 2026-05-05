c2f1bc002bc52c6644a909f7a702b84bd7a0a883
Zoey Chen <zixiu.zoey.chen@gmail.com>
2026-05-05 08:50:27 -0400

    changed things intellij was upset at me about

--- mushroot-backend/src/main/java/mushroot/mushrootbackend/repository/UserRepository.java
+++ mushroot-backend/src/main/java/mushroot/mushrootbackend/repository/UserRepository.java
@@ -1,3 +1,11 @@
-public class UserRepository {
+package mushroot.mushrootbackend.repository;
+
+import org.apache.catalina.User;
+import org.springframework.data.repository.CrudRepository;
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+public interface UserRepository extends CrudRepository<User, Integer> {
     
 }
