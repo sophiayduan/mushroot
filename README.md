335aec252f9e87f95e265315edd6e736c38f75b8
AnnieL5 <alian5@ocdsb.ca>
2026-05-04 23:17:59 -0400

    User.java for building the database

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 run ``npm install``
 
 run ``npm run dev``
+
+
+## To run
+
+1. 
+```SQL
+CREATE DATABASE mushroot;
+```
---
+++ mushroot-backend/src/main/java/mushroot/mushrootbackend/model/User.java
@@ -0,0 +1,33 @@
+package mushroot.mushrootbackend.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data               // Lombok magic: auto-generates getters, setters, toString
+@Entity             // Tells JPA: "this is a database table"
+@Table(name = "users")  // The table will be called "users" in MySQL
+public class User {
+
+    @Id                                          // This is the primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  // Auto-increment (1, 2, 3...)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)     // Can't be empty, must be unique
+    private Course_Code course_code;
+
+    @Column(nullable = false)
+    private int year;
+
+    @Column(nullable = false)
+    private String teacher_name;
+
+    @Lab
+    @Column(nullable = false, columnDefinition = "MEDIUMBLOB")
+    private byte[] data;
+
+}
+
+public enum Course_Code {
+    MDM4U, SCH4U, SPH4U, ICS4U
+}
