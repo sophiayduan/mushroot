7dc0d1abbe15a5140a27f9b11eecd0bb6076c0ba
Erin <enguy4@ocdsb.ca>
2026-05-06 10:18:13 -0400

    Accessibility stuff locked in ermm

---
+++ app/routes/ac/accesibility.tsx
@@ -0,0 +1,16 @@
+
+function accesibility() {
+    return (
+        <main className="bg-[#FAF4E9] h-screen flex flex-col items-center">
+            <div className = "flex flex-col bg-[#F3CBC5] min-h-30 min-w-300 mt-5 text-center justify-center rounded-xl">
+                <h1 className="text-[#FAF4E9] font-margarine text-3xl"> Accesibility </h1>
+            </div>
+
+            <div className = "grid grid-cols-2 grid-rows-3"></div>
+            <p>jkdfgljasdljfdasjlkafsdlj</p>
+
+        </main>
+    );
+}
+
+export default accesibility;
