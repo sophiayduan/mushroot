b2b4a090fce418d7bf9deaac617077fab6d17466
Erin <enguy4@ocdsb.ca>
2026-05-06 11:06:27 -0400

    pls work

--- app/routes.ts
+++ app/routes.ts
@@ -2,6 +2,6 @@ import { type RouteConfig, route, index } from "@react-router/dev/routes";
 
 export default [
     index("routes/ac/accesibility.tsx"),
-    route("dashboard", "routes/dashboard/dashboard.tsx"),
+    route("dashboard", "routes/merge/merge.tsx"),
 
 ] satisfies RouteConfig;
