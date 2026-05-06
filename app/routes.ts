4260ee9a86460644435155f4461510f651478fce
Erin <enguy4@ocdsb.ca>
2026-05-06 10:18:13 -0400

    Accessibility stuff locked in ermm

--- app/routes.ts
+++ app/routes.ts
@@ -1,6 +1,7 @@
 import { type RouteConfig, route, index } from "@react-router/dev/routes";
 
 export default [
-    index("routes/hero/hero.tsx"),
+    index("routes/ac/accesibility.tsx"),
     route("dashboard", "routes/dashboard/dashboard.tsx"),
+
 ] satisfies RouteConfig;
