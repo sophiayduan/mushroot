16d110f309e83dd52a4287da566f35bd6e8f1777
Nayraiyt <naomimorganc@gmail.com>
2026-05-07 09:05:33 -0400

    added route

--- app/routes.ts
+++ app/routes.ts
@@ -1,7 +1,7 @@
 import { type RouteConfig, route, index } from "@react-router/dev/routes";
 
 export default [
-    index("routes/ac/accesibility.tsx"),
+    index("routes/upload/hero.tsx"),
     route("dashboard", "routes/merge/merge.tsx"),
 
 ] satisfies RouteConfig;
