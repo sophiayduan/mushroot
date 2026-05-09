b0a268b6ebc27c4049620abb64b06f86c43183cb
salma-youssef9 <salmayoussefmarch@gmail.com>
2026-05-09 17:35:47 -0400

    route fix and upload page edits

--- app/routes.ts
+++ app/routes.ts
@@ -1,7 +1,11 @@
-import { type RouteConfig, route, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
+
     index("routes/upload/uploadpage.tsx"),
-    route("upload", "routes/upload/uploadpage.tsx"),
-    route("dashboard", "routes/merge/merge.tsx"),
+
+    route("upload", "routes/upload/uploadpage.tsx", { id: "upload-route" }),
+
+    route("dashboard", "routes/merge/merge.tsx", { id: "dashboard-route" }),
+
 ] satisfies RouteConfig;
