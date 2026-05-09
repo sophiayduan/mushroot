c9e021e1069d3f6119d452c385181b6403d94040
Nayraiyt <naomimorganc@gmail.com>
2026-05-09 19:01:10 -0400

    changed name

--- app/routes/upload/UploadPage.tsx
+++ app/routes/upload/UploadPage.tsx
@@ -1,7 +1,7 @@
 // uploadpage.tsx
 
 import React, { useRef, useState } from "react";
-import Navbar from "../../components/NavBar/Navbar";
+import Navbar from "../../components/navbar/Navbar";
 import styles from "./uploadpage.module.css";
 
 interface UploadPageProps {
