de57686801b395afe8ec26a3357964a842257c7d
salma-youssef9 <salmayoussefmarch@gmail.com>
2026-05-10 00:32:47 -0400

    fix routes + archives

--- app/routes.ts
+++ app/routes.ts
@@ -1,14 +1,10 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
-
     index("routes/hero/hero.tsx"),
-    
-    route("accesibility","routes/ac/accesibility.tsx", {id: "accesibility-route"}),
+    route("accesibility", "routes/ac/accesibility.tsx", { id: "accesibility-route" }),
     route("archive", "routes/archive/archive.tsx", { id: "archive-route" }),
     route("hero", "routes/hero/hero.tsx", { id: "hero-route" }),
     route("merge", "routes/merge/merge.tsx", { id: "merge-route" }),
-    route("upload", "routes/archive/upload.tsx", { id: "upload-route" }),
-    route("timer", "routes/timer/timer.tsx")
-
+    route("upload", "routes/upload/UploadPage.tsx", { id: "upload-route" }),
 ] satisfies RouteConfig;
