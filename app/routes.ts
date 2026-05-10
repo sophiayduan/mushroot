3caf6c9f40bf9272c4bb42a25127a21d4dd57215
Nayraiyt <naomimorganc@gmail.com>
2026-05-09 23:21:34 -0400

    Added to GitIgnore for VS Code (dont wanna leak db passwords lol)

--- app/routes.ts
+++ app/routes.ts
@@ -3,11 +3,12 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
 
     index("routes/hero/hero.tsx"),
+    
     route("accesibility","routes/ac/accesibility.tsx", {id: "accesibility-route"}),
     route("archive", "routes/archive/archive.tsx", { id: "archive-route" }),
     route("hero", "routes/hero/hero.tsx", { id: "hero-route" }),
     route("merge", "routes/merge/merge.tsx", { id: "merge-route" }),
-    route("upload", "routes/upload/uploadpage.tsx", { id: "upload-route" }),
+    route("upload", "routes/archive/upload.tsx", { id: "upload-route" }),
     route("timer", "routes/timer/timer.tsx")
 
 ] satisfies RouteConfig;
