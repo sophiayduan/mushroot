61597bb6f09e97da65c4aff0a3096d635f5d8468
Nayraiyt <naomimorganc@gmail.com>
2026-05-10 15:16:03 -0400

    Changed upload path, added to merge game

--- app/routes/merge/merge.tsx
+++ app/routes/merge/merge.tsx
@@ -1,7 +1,7 @@
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import MergeLogic from "./MergeGame";
-import Navbar from "../../components/navbar/Navbar";
+import Navbar from "../../components/NavBar/Navbar";
 
 function Merge() {
   return (
